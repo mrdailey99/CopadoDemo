4cebae08dfda0818a9ac9c22fb76a22b483a0668
Michael Dailey <michael.dailey@provartesting.com>
2025-11-21 08:22:57 -0600

    updated to point to scratch org for testing grid video bug (pmqhbug)

---
+++ src/pageobjects/MyPageObject.java
@@ -0,0 +1,21 @@
+package pageobjects;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.provar.core.testapi.annotations.*;
+
+@Page( title="MyPageObject"                                
+     , summary=""
+     , relativeUrl=""
+     , connection="Admin"
+     )             
+public class MyPageObject {
+
+	@ButtonType()
+	@FindBy(xpath = "//div[contains(@class, 'active') and contains(@class, 'open') and (contains(@class, 'forceModal') or contains(@class, 'uiModal'))][last()]//button[normalize-space(.)='Done']")
+	public WebElement done;
+			
+}
